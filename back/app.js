04f40600454a3f83fe5c270bf860c66b7801b617
hailie <20181011@sungshin.ac.kr>
2021-10-02 19:22:03 +0900

    [update] room, game 미들웨어 수정 및 1차 완성

--- back/app.js
+++ back/app.js
@@ -8,6 +8,7 @@ const {
     waitingRoomRouter,
     gameRouter,
 } = require('./router');
+const { authMiddleware, roomMiddleware, gameMiddleware } = require('./middleware');
 //const { authMiddleware } = require('./middleware')
 const PORT = 8080;
 
@@ -19,6 +20,41 @@ app.get('/', (req, res) => {
     res.send('success');
 });
 app.use(express.json());
+
+app.get('/test1', authMiddleware, gameMiddleware, (req,res) => {
+    const user = res.locals.user
+    const leader = res.locals.leader
+    const role = res.locals.role
+    res.json({
+        user:user.user_idx, leader, role
+    });
+} )
+// gameSetIdx
+app.get('/test2', authMiddleware, gameMiddleware, (req,res) => {
+    const user = res.locals.user
+    const leader = res.locals.leader
+    const role = res.locals.role
+    res.json({
+        user:user.user_idx, leader, role
+    });
+} )
+app.get('/test1/:gameIdx', authMiddleware, gameMiddleware, (req,res) => {
+    const user = res.locals.user
+    const leader = res.locals.leader
+    const role = res.locals.role
+    res.json({
+        user:user.user_idx, leader, role
+    });
+} )
+app.get('/test2/:gameSetIdx', authMiddleware, gameMiddleware, (req,res) => {
+    const user = res.locals.user
+    const leader = res.locals.leader
+    const role = res.locals.role
+    res.json({
+        user:user.user_idx, leader, role
+    });
+} )
+
 app.use('/login', loginRouter);
 app.use('/room', roomRouter);
 app.use('/waiting-room', waitingRoomRouter);
--- back/middleware/game.js
+++ back/middleware/game.js
@@ -1,21 +1,63 @@
-const { User } = require('../models');
+const { User, Game, GameSet, GameMember, WaitingRoomMember } = require('../models');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     // game_set_idx, game_idx와 user 정보 가지고 방장/방원/마피아/시민인지 체크
     try {
         const user = res.locals.user;
         const { game_set_idx, game_idx } = req.body;
-        const { gameSetIdx, gameIdx } = req.param;
+        const { gameSetIdx, gameIdx } = req.params;
 
-        // db 처리
+        
+        let realGameSetIdx = (!game_set_idx) ? gameSetIdx : game_set_idx;
+        let realGameIdx = (!game_idx) ? gameIdx : game_idx;
 
-        res.locals.leader = true; // or false
-        res.locals.role = "ghost"; // or "human"  
+        if ( !realGameIdx ){
+            const gameSet = await GameSet.findOne({
+                attributes: [ 'game_game_idx' ],
+                where: {
+                    game_set_idx: realGameSetIdx
+                }
+            });
+            if(!gameSet){
+                res.status(400).send({
+                    message: '존재하는 게임이 아닙니다.',
+                });
+                return;
+            }
+            realGameIdx = gameSet.game_game_idx;
+        }
+
+        // SELECT GameMember.*, WaitingRoomMember.wrm_leader FROM GameMember INNER JOIN WaitingRoomMember ON wrm_idx = wrm_wrm_idx
+        // WHERE game_game_idx =1 AND wrm_user_idx = 1;
+        const gameMember = await GameMember.findOne({
+            include: [
+                {
+                    model: WaitingRoomMember,
+                    as : 'wrm_wrm_idx_WaitingRoomMember',
+                    required: false,
+                    attributes: ['wrm_leader']
+                },
+            ],
+            where:{
+                game_game_idx: realGameIdx,
+                wrm_user_idx: user.user_idx
+            }
+        });
+
+        if(!gameMember){
+            res.status(403).send({
+                message: '대기실/게임의 참여자가 아닙니다.',
+            });
+            return;
+        }
+
+        res.locals.leader = gameMember.wrm_wrm_idx_WaitingRoomMember.wrm_leader;
+        res.locals.role = gameMember.game_member_role; 
         next();
     } catch (error) {
         console.log(error);
-        res.status(403).send({
-            message: '게임의 참여자가 아닙니다.',
+        res.status(400).send({
+            message: '알 수 없는 에러가 발생하였습니다.',
         });
         return;
     }
