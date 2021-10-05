db24cc93275de5dc2b4c3a2d51c8b5bb59df83a5
sunga0101 <2238016@naver.com>
2021-10-05 14:10:36 +0900

    [update] style 구조 세팅

--- front/hollymolly/src/App.js
+++ front/hollymolly/src/App.js
@@ -1,30 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import style from './styles/styles';
+import style from './styles/styles.js';
 
 function App() {
-    /* return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    홀리몰리의 세계에 오신 것을 환영합니다!
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    ); */
     return (
-        <div style={{background: style.black}}>
-                <p>
-                    {/* Edit <code>src/App.js</code> and save to reload.
-                     */}
-                    홀리몰리의 세계에 오신 것을 환영합니다!
-                </p>
+        <div style={{ backgroundColor: style.gray }} className="App">
+            {' '}
+            // import한 styles.js 사용할 때
+            <div style={styles.dd}>
+                {' '}
+                // 아래에서 만든 스타일 변수 사용할 때<h1>안녕하세요!</h1>
+                <hr style={{ width: '100%' }} />
+                <p style={styles.text}>이름을 입력해주세요.</p> 아래에서 만든 스타일 변수 사용할 때
+                <input type="text" />
+            </div>
         </div>
     );
 }
 
 export default App;
+
+const styles = {
+    dd: {
+        border: '2px solid #eee',
+        padding: '20px',
+        display: 'flex',
+        width: '100vw',
+        maxWidth: '400px', // 400픽셀 이상으로는 커지지 않는다.
+        margin: '30px auto',
+        flexDirection: 'column',
+    },
+    text: {
+        color: style.red,
+    },
+};
