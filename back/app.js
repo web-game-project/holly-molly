30fcad1dae531e50692837c5fa715f40d252dd99
hailie <20181011@sungshin.ac.kr>
2021-10-08 01:24:47 +0900

    [add] chat 기능 1차개발 완료, 마피아 제시어 입력/세트 시작 ~ing

--- back/app.js
+++ back/app.js
@@ -9,7 +9,8 @@ const {
     gameRouter,
 } = require('./router');
 const { authMiddleware, roomMiddleware, gameMiddleware } = require('./middleware');
-const PORT = 3002;
+//const PORT = 8080; 
+const PORT = 3002
 
 const app = express();
 const server = Http.createServer(app);
---
+++ back/service/game/writeHumanKeyword.js
@@ -0,0 +1,39 @@
+const { GameSet } = require('../../models');
+const moveRoom = require('../../socket/moveRoom');
+
+module.exports = async (req, res, next) => {
+    try {
+        const { game_set_idx, game_set_no, game_set_human_answer } = req.body;
+
+        // 마피아인지 아닌지 체크
+        if (res.locals.role != 'human'){
+            res.status(403).json({ meesage: '권한이 없습니다.' });
+            return;
+        }
+
+        // 마피아 제시어 입력
+        const gameSet = await GameSet.update(
+            { game_set_human_answer },
+            {
+                where: { game_set_idx, game_set_no },
+            }
+        );
+        console.log(gameSet);
+        if(!gameSet){
+            res.status(400).json({ meesage: '알 수 없는 에러가 발생했습니다.' });
+            return;
+        }
+
+        // room 번호 찾기
+
+
+        // 소켓 submit mafia answer , mafia_submit:true
+        const io = req.app.get('io');
+        io.to(roomIdx).emit('submit mafia answer', { mafia_submit: trye });
+
+        res.status(200).json({});
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ meesage: '알 수 없는 에러가 발생했습니다.' });
+    }
+};
