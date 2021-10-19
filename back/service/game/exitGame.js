98cd7130b19d9394116897a750e483a680766a8d
hailie <20181011@sungshin.ac.kr>
2021-10-19 23:55:50 +0900

    [add] 비정상 종료 로직

--- back/service/game/exitGame.js
+++ back/service/game/exitGame.js
@@ -1,23 +1,56 @@
 const { Room, Game, GameSet, GameMember, GameVote } = require('../../models');
 const moveRoom = require('../../socket/moveRoom');
 
-module.exports = async (req, res, next) => {
+const exitGame = async (req, res, next) => {
     try {
-        // user와 관련된 정보들 get
+        const user = res.locals.user;
+        const { game, gameMember } = await getGameAndMember(user.user_idx);
+        const { room, roomMember } = await getRoomAndMember(user.user_idx);
+        const memberList = await getMemberList(room.get('room_idx')); //roomMember+gameMemberIdx
+        const isLeader = roomMember.get('wrm_leader');
+        const io = req.app.get('io');
 
-        // game 중이면, 방장/마피아/시민 정보 확인
+        if(memberList.size()==1){
+            await deleteAllAboutGame(memberList, game.get('game_idx')); // game, gameMember, gameSet, gameVote 삭제
+            await deleteRoomAndMember(roomMember.get('wrm_idx'), room.get('room_idx')); // room, roomMember 삭제
+            await deleteUser(user.user_idx);
 
-        // 방장 권한 옮기기
+            io.to(0).emit('delete room', { room_idx: room.get('room_idx')});
+            res.status(204).end();
+            return;
+        }
 
-        // 마피아이면 게임 끝내기
+        if(game){
+            if(gameMember.get('game_member_role')=='human'){
+                // 게임 종료 처리 (game, gameMember, gameSet, gameVote 삭제)
+                await deleteAllAboutGame(memberList, game.get('game_idx'));
+                await deleteUser(user.user_idx);
 
-        // 게임에서 나가기 (투표는 냅두기)
+                // 최종 결과 이벤트
+                const finalResult = await getFinalResult(game.get('game_idx'));
+                io.to(room.get('room_idx')).emit('get final result', finalResult);
 
-        // 대기실에서 나가기
+                // game status 이벤트
+                await updateRoomStatus(room.get('room_idx'));
+                io.to(0).emit('change game status', { room_idx:room.get('room_idx'), room_status:"waiting"});
+            }
+        } 
 
-        // user 정보 없애기
+        if(isLeader){
+            const hostIdx = changeHost(memberList, user.user_idx);
+            io.to(0).emit('change host', { user_idx: hostIdx });
+        }
 
-        // 소켓 - 게임 인원 바뀜 표시 (대기실 리스트 & 대기실)
+        if(gameMember){
+            await deleteGameMember(gameMember.game_member_idx);
+        }
+        if(roomMember){
+            await deleteRoomMember(roomMember.wrm_idx);
+        }
+        io.to(0).emit('change member count', { room_idx: room.get('room_idx'), room_member_count: memberList.size()-1 });
+        io.to(room.get('room_idx')).emit('exit room', {user_idx: user.user_idx});
+
+        await deleteUser(user.user_idx);
 
         res.status(204).end();
     } catch (error) {
@@ -26,7 +59,20 @@ module.exports = async (req, res, next) => {
     }
 };
 
-const deleteGame = async (gameMembers, gameIdx) => {
+const getGameAndMember = async (userIdx) => {
+
+}
+const getRoomAndMember = async (userIdx) => {
+
+}
+const getMemberList = async (roomIdx) => {
+
+}
+const deleteRoomAndMember = async (wrmIdx, roomIdx) => {
+
+}
+
+const deleteAllAboutGame = async (gameMembers, gameIdx) => {
     let gameMemberIdx = [];
     for (member of gameMembers) {
         gameMemberIdx.push(member.get('game_member_idx'));
@@ -53,3 +99,39 @@ const deleteGame = async (gameMembers, gameIdx) => {
         },
     });
 };
+
+const deleteUser = async (userIdx) => {
+
+}
+
+const getFinalResult = async (gameIdx) => {
+
+}
+
+const updateRoomStatus = async (roomIdx) => {
+    await Room.update(
+        { room_status: 'waiting' },
+        {
+            where: {
+                room_idx: roomIdx,
+            },
+        }
+    );
+}
+
+const changeHost = async (memberList, userIdx) => {
+
+}
+
+const deleteGameMember = async (gameMemberIdx) => {
+
+}
+
+const deleteRoomMember = async (wrmIdx) => {
+
+}
+
+module.exports = {
+    exitGame,
+
+}
