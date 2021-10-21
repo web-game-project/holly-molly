aafc8e876d71ac1985a7bca6306dfeb062127134
hailie <20181011@sungshin.ac.kr>
2021-10-21 21:35:24 +0900

    [update] 인간 답안 제출 및 제출 완료 1차 개발

--- back/service/game/writeHumanKeyword.js
+++ back/service/game/writeHumanKeyword.js
@@ -1,9 +1,9 @@
-const { GameSet } = require('../../models');
+const { GameSet, Game } = require('../../models');
 const moveRoom = require('../../socket/moveRoom');
 
 module.exports = async (req, res, next) => {
     try {
-        const { game_set_idx, game_set_no, game_set_human_answer } = req.body;
+        const { game_set_idx, game_set_human_answer } = req.body;
 
         // 인간인지 아닌지 체크
         if (res.locals.role != 'human') {
@@ -12,25 +12,30 @@ module.exports = async (req, res, next) => {
         }
 
         // 인간 제시어 입력
-        const gameSet = await GameSet.update(
-            { game_set_human_answer },
-            {
-                where: { game_set_idx, game_set_no },
-            }
-        );
-        console.log(gameSet);
+        const gameSet = await GameSet.findOne({
+            where: { game_set_idx },
+        });
         if (!gameSet) {
             res.status(400).json({
                 meesage: '알 수 없는 에러가 발생했습니다.',
             });
             return;
         }
+        gameSet.game_set_human_answer = game_set_human_answer;
+        await gameSet.save();
 
-        // room 번호 찾기
+        // room 번호 찾기 
+        const game = await Game.findOne({
+            attributes: ['room_room_idx'],
+            where: {
+                game_idx: gameSet.game_game_idx,
+            }
+        }) 
 
         // 소켓 submit mafia answer , mafia_submit:true
+        console.log(game.get('room_room_idx'));
         const io = req.app.get('io');
-        io.to(roomIdx).emit('submit human answer', { mafia_submit: true });
+        io.to(game.get('room_room_idx')).emit('submit human answer', { mafia_submit: true });
 
         res.status(201).json({});
     } catch (error) {
