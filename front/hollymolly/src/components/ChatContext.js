a33fe0c02fd98f16d4ca4b8a9892c5d3897ded4d
JungYeonHee <dusgml5328@gmail.com>
2021-10-23 17:53:42 +0900

    [chore] 문자 길이에 맞게 채팅 ui 수정 및 기본 폰트 전체 적용

--- front/hollymolly/src/components/ChatContext.js
+++ front/hollymolly/src/components/ChatContext.js
@@ -3,22 +3,13 @@ import styled from "styled-components";
 
 const ChatContext = (props) => {
     const {name, text, color, children} = props;
-    
-
     const styles = {
       color: color,
      };
 
-    const nameLength = props.name.length * 16;
-    console.log(nameLength);
-
-    
-
       return (
         <React.Fragment>
-          <Container>
-              <ChatContextBox {...styles}><ChatUserNameBox style={{color : color, width : nameLength}}>{name}:</ChatUserNameBox> {text}</ChatContextBox>
-          </Container>
+              <ChatContextBox {...styles}>{name} : {text}</ChatContextBox>
         </React.Fragment>
       );
   };
@@ -27,34 +18,18 @@ const ChatContext = (props) => {
     chidren: null,
   };
 
-  const Container = styled.div`
-    width: 220px;
-    height: 20px; 
-    margin-bottom: 5px;
-    display: flex; 
-    align-items: center; 
-    justify-content: flex-start;
-  `;
-
-  const ChatUserNameBox = styled.div`
-    width: 220px;
-    height: 20px;    
-    background-color: #B0B0B0;
-    border-radius: 0.5rem;
-    color: ${(props) => props.color};
-    font-size: 15px;
-  `;
-
   const ChatContextBox = styled.div`
     width: 220px;
-    height: 20px;    
     background-color: #B0B0B0;
     border-radius: 0.5rem;
     font-size: 15px;
-    display: flex; 
-    align-items: flex-start; 
-    justify-content: flex-start;
-    flex-direction: row;
+    color: ${(props) => props.color};
+    font-family: 'Gowun Dodum';
+    font-weight: bold;
+    display: flex
+    flex-flow: row wrap;
+    margin-bottom: 5px;
+   
   `;
 
   export default ChatContext;
--- front/hollymolly/src/index.js
+++ front/hollymolly/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./static/fonts/font.css";
+import '../src/assets/font.css'
 
 ReactDOM.render(
   <React.StrictMode>
