c88edb7be7d59361ac2dd75c435281b5721dd2af
hailie <20181011@sungshin.ac.kr>
2021-10-25 22:31:43 +0900

    [fix] roomMiddleware 위치 변경

--- back/router/room.js
+++ back/router/room.js
@@ -11,11 +11,11 @@ const { authMiddleware, roomMiddleware } = require('../middleware');
 
 const router = express.Router();
 
-router.get('/', authMiddleware, getRoomList);
-router.post('/', authMiddleware, makeRoom);
-router.post('/:type', authMiddleware, enterRoom);
+router.get('/', getRoomList);
+router.post('/', makeRoom);
+router.post('/:type', enterRoom);
 router.get('/info/:roomIdx', roomMiddleware, getRoomInfo);
-router.put('/info', roomMiddleware, roomMiddleware, editRoomInfo);
+router.put('/info', roomMiddleware, editRoomInfo);
 router.delete('/:roomIdx', roomMiddleware, deleteRoom);
 
 module.exports = router;
