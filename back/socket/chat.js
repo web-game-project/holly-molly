f694f40bf5a200ac337cb52cffdec44e016bbd77
hailie <20181011@sungshin.ac.kr>
2021-10-26 22:31:07 +0900

    [fix] 소켓 미들웨어

--- back/socket/chat.js
+++ back/socket/chat.js
@@ -3,7 +3,7 @@ module.exports = async (socket, io, data) => {
         user_idx: data.user_idx,
         user_name: data.user_name,
         msg: data.msg,
-    })
+    });
     /*socket.broadcast.to(data.room_idx).emit('chat', {
         user_idx: data.user_idx,
         user_name: data.user_name,
