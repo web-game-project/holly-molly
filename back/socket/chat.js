84d19e6f171a741be54772754cce25c5805e61ab
hailie <20181011@sungshin.ac.kr>
2021-10-26 23:48:23 +0900

    [fix] 테스트용 코드 추가

--- back/socket/chat.js
+++ back/socket/chat.js
@@ -1,4 +1,5 @@
 module.exports = async (socket, io, data) => {
+    console.log("*****소켓 테스트 중2", socket.id, date);
     io.to(data.room_idx).emit('chat', {
         user_idx: data.user_idx,
         user_name: data.user_name,
