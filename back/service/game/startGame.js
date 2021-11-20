cfdb0177514801bcf2ac959efca3fc6a493d2af0
jyeon1039 <20181039@sungshin.ac.kr>
2021-11-20 11:09:24 +0900

    [update] 400 error 관련 변경

--- back/service/game/startGame.js
+++ back/service/game/startGame.js
@@ -40,6 +40,10 @@ module.exports.startGame = async (req, res, next) => {
         res.status(200).json("success");
     } catch (error) {
         console.log('startGame Error: ', error);
+        res.status(400).json({
+            meesage: '알 수 없는 에러가 발생했습니다.',
+            error,
+        });
     }
 };
 
--- back/service/waitingRoom/changeUserReady.js
+++ back/service/waitingRoom/changeUserReady.js
@@ -19,6 +19,10 @@ module.exports = async (req, res, next) => {
 
         res.status(200).json('success');
     } catch (error) {
-        console.log('***********', error);
+        console.log('changeUserReady Error:', error);
+        res.status(400).json({
+            meesage: '알 수 없는 에러가 발생했습니다.',
+            error,
+        });
     }
 };
