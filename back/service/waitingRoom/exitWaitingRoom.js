50216402f08a3574eb1367f277cee1415ccc9e88
jyeon1039 <20181039@sungshin.ac.kr>
2021-11-22 22:53:23 +0900

    [update] start game 때 db에서 게임 상태 변경, 400 에러 메시지 수정

--- back/service/waitingRoom/exitWaitingRoom.js
+++ back/service/waitingRoom/exitWaitingRoom.js
@@ -61,7 +61,7 @@ module.exports = async (req, res, next) => {
         console.log('exitWaitingRoom Error: ', error);
         res.status(400).json({
             meesage: '알 수 없는 에러가 발생했습니다.',
-            error,
+            error: error.message,
         });
     }
 };
