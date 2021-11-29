d5fd206f4dd3d746ff92e9c7c931bb3b7346e4a7
JungYeonHee <dusgml5328@gmail.com>
2021-11-30 01:50:31 +0900

    [chore] 리덕스 적용 및 소켓 에러 수정
    - 리덕스에 있는 baseURL로 Rest API url 수정
    - 소켓 여러번 오는 버그 수정 (전역으로 두었던 소켓 연결을 useEffect 빈 배열로 이동)

--- front/hollymolly/src/screens/RoomList.js
+++ front/hollymolly/src/screens/RoomList.js
@@ -8,7 +8,6 @@ import Filter from "../components/Filter";
 import ModalBase from "../components/ModalBase";
 import { useHistory } from "react-router";
 
-// import Child from '../components/Child';
 import RefreshVerification from "../server/RefreshVerification";
 
 // 이미지
@@ -17,6 +16,10 @@ import rightArrowBtn from "../assets/rightArrowBtn.png";
 
 // 소켓
 import { io } from "socket.io-client";
+
+// 리덕스 & 로딩
+import { useSelector } from "react-redux";
+import Loading from "../components/Loading";
 
 let total_room_cnt = 0; // 룸 리스트 총 방의 갯수
 
@@ -32,22 +35,17 @@ let save_user_name = JSON.parse(data) && JSON.parse(data).user_name;
 console.log("save_token: " + save_token);
 console.log("save_user_name: " + save_user_name);
 
-const socket = io("http://3.17.55.178:3002/", {
-  // 프론트가 서버와 동일한 도메인에서 제공되지 않는 경우 서버의 URL 전달 필요
-  auth: {
-    // 1번 토큰
-    token: save_token,
-  },
-});
-
 const RoomList = () => {
   const history = useHistory();
 
+  // 리덕스에 저장된 값
+  const isLogin = useSelector((state) => state.socket.is_login);
+  const baseURL = useSelector((state) => state.socket.base_url);
+
   const [emptyRoomsLength, setEmptyRoomsLength] = useState("");
-
   const [createRoomData, setcreateRoomData] = useState("");
-
   const [isSocket, setIsSocket] = useState(false);
+  const [currentSocketConnection, setCurrentSocketConnection] = useState();
 
   // 방 전체 리스트
   const [rooms, setRooms] = useState();
@@ -61,35 +59,60 @@ const RoomList = () => {
   const resultArray = result.sort();
 
   useEffect(() => {
-    socket.on("connect", () => {
-      console.log("room list connection server");
+    const socket = io("http://3.17.55.178:3002/", {
+      auth: {
+        token: save_token,
+      },
     });
 
-    socket.on("error", () => {
-      setTimeout(() => {
-        socket.connect();
-        console.log(socket);
-      }, 1000);
+    socket.on("connect", () => {
+      console.log("room list connection server");
+      setCurrentSocketConnection(socket.connected);
     });
 
     // 연결 해제 시 임의 지연 기다린 다음 다시 연결 시도
     socket.on("disconnect", (reason) => {
-      if (reason === "io server disconnect") {
-        socket.connect();
-      }
+      console.log("disconnect");
+      setCurrentSocketConnection(socket.connected);
     });
-  });
 
-  // 맨 처음에 첫 페이지 부름
-  useEffect(() => {
-    roomListCheckPage(0);
+    //방 생성 시, 마지막 페이지에 방 추가
+    socket.on("create room", (data) => {
+      //setcreateRoomData(data);
+      console.log("create room");
+      setIsSocket(!isSocket);
+    });
+
+    // 방 삭제 - 대기실 삭제
+    socket.on("delete room", (data) => {
+      console.log("delete room");
+      setIsSocket(!isSocket);
+    });
+
+    //방 정보 수정  - 특정 대기실에서 대기실 정보 수정 시
+    socket.on("edit room", (data) => {
+      console.log("edit room");
+      setIsSocket(!isSocket);
+    });
+
+    // 방 멤버 변동 - 특정 대기실 사용자 입장/퇴장 시
+    socket.on("change member count", (data) => {
+      console.log("change member count");
+      setIsSocket(!isSocket);
+    });
+
+    //방 상태 변동 - 특정 게임이 시작할 때
+    socket.on("change game status", (data) => {
+      console.log("change game status");
+      setIsSocket(!isSocket);
+    });
   }, []);
 
-  // 소켓 이벤트 발생 시 부름
+  // 소켓 이벤트 발생 시 모든 페이지 다시 부름
   useEffect(() => {
-    /* for (let i = 0; i < TOTAL_SLIDES; i++) {
-                roomListCheckPage(i);
-        } */
+     for (let i = 0; i < TOTAL_SLIDES; i++) {
+        roomListCheckPage(i);
+      } 
   }, [isSocket]);
 
   // 페이지 슬라이드 개수
@@ -122,40 +145,10 @@ const RoomList = () => {
     }
   };
 
-  useEffect(() => {
-    //방 생성 시, 마지막 페이지에 방 추가
-    socket.on("create room", (data) => {
-      setcreateRoomData(data);
-      console.log("create room");
-    });
-
-    // 방 삭제 - 대기실 삭제
-    socket.on("delete room", (data) => {
-      console.log("delete room");
-    });
-
-    //방 정보 수정  - 특정 대기실에서 대기실 정보 수정 시
-    socket.on("edit room", (data) => {
-      console.log("edit room");
-    });
-
-    // 방 멤버 변동 - 특정 대기실 사용자 입장/퇴장 시
-    socket.on("change member count", (data) => {
-      console.log("change member count");
-      //console.log(data);
-      //setIsSocket(!isSocket);
-    });
-
-    //방 상태 변동 - 특정 게임이 시작할 때
-    socket.on("change game status", (data) => {
-      console.log("change game status");
-    });
-  });
-
   // 페이지별 룸 리스트 조회
   const roomListCheckPage = async (currentPage) => {
     currentPage = currentSlide + 1;
-    var restURL = "http://3.17.55.178:3002/room?page=" + currentPage;
+    var restURL = baseURL + "room?page=" + currentPage;
     restURL = filterUrl(restURL, resultArray);
 
     const reqHeaders = {
@@ -181,7 +174,7 @@ const RoomList = () => {
     // 룸 리스트 조회
     const roomListCheck = async () => {
       const currentPage = currentSlide + 1;
-      var restURL = "http://3.17.55.178:3002/room?page=" + currentPage;
+      var restURL = baseURL + "room?page=" + currentPage;
       restURL = filterUrl(restURL, resultArray);
 
       const reqHeaders = {
@@ -199,7 +192,7 @@ const RoomList = () => {
           setEmptyRoomsLength(6 - response.data.room_list.length); // empty room list length
         })
         .catch(function (error) {
-          console.log(error.data);
+          alert(error.response.data.message);
         });
     };
 
@@ -295,7 +288,7 @@ const RoomList = () => {
 
     console.log(modeFilterArray);
     console.log(personFilterArray);
-    const reqURL = "http://3.17.55.178:3002/room/random"; //parameter : 방 타입
+    const reqURL = baseURL + "room/random"; //parameter : 방 타입
     const reqHeaders = {
       headers: {
         authorization: "Bearer " + save_token,
@@ -327,105 +320,122 @@ const RoomList = () => {
   return (
     <React.Fragment>
       <Background>
-        <RoomGrid
-          flexDirection="column"
-          padding="20px"
-          width="1020px"
-          height="620px"
-          bg="#DAD4F6"
-        >
-          {/* 검색바 & 버튼 div*/}
+        {currentSocketConnection ? (
           <RoomGrid
-            is_flex_space
-            width="980px"
-            height="110px"
+            flexDirection="column"
+            padding="20px"
+            width="1020px"
+            height="620px"
             bg="#DAD4F6"
-            border="1px solid #DAD4F6"
           >
-            <div style={styles.grid}>
-              <RoomSearchBar />
-            </div>
-            {/* 버튼 div*/}
-            <div
-              style={{
-                flexDirection: "column",
-                width: "220px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
+            {/* 검색바 & 버튼 div*/}
+            <RoomGrid
+              is_flex_space
+              width="980px"
+              height="110px"
+              bg="#DAD4F6"
+              border="1px solid #DAD4F6"
             >
-              {/* 방만들기 모달 */}
-              <ModalBase />
-              <br />
-              <Button onClick={randomEntry}>랜덤 입장</Button>
-            </div>
-          </RoomGrid>
-
-          {/* 방 & 필터 div*/}
-          <RoomGrid
-            is_flex_space
-            width="980px"
-            height="460px"
-            bg="#DAD4F6"
-            border="1px solid #DAD4F6"
-          >
-            {/* 왼쪽 화살표 div*/}
-            <PrevBtn onClick={prevPage} />
-            {/* 방 리스트 슬라이더 div*/}
-            <div style={styles.sliderContainer}>
-              <div style={styles.roomListContainer}>
-                {rooms &&
-                  rooms.room_list.map((values) => {
-                    return values.room_status === "waiting" ? (
-                      <Room
-                        room_idx={values.room_idx}
-                        room_name={values.room_name}
-                        room_current_member={values.room_current_member_cnt}
-                        room_start_member={values.room_start_member_cnt}
-                        room_mode={values.room_mode}
-                        room_status={values.room_status}
-                        disabled="false"
-                        textStroke="true"
-                        cursor="true"
-                      />
-                    ) : (
-                      <Room
-                        room_idx={values.room_idx}
-                        room_name={values.room_name}
-                        room_current_member={values.room_current_member_cnt}
-                        room_start_member={values.room_start_member_cnt}
-                        room_mode={values.room_mode}
-                        room_status={values.room_status}
-                        disabled="true"
-                        textStroke="true"
-                        cursor="false"
-                      />
-                    );
-                  })}
-                {createRoomData && (
-                  <Room
-                    room_idx={createRoomData.room_idx}
-                    room_name={createRoomData.room_name}
-                    room_current_member={createRoomData.room_current_member_cnt}
-                    room_start_member={createRoomData.room_start_member_cnt}
-                    room_mode={createRoomData.room_mode}
-                    room_status={createRoomData.room_status}
-                    disabled="false"
-                    textStroke="true"
-                    cursor="true"
-                  />
-                )}
-                {emptyRoomList()}
+              <div style={styles.grid}>
+                <RoomSearchBar />
               </div>
-              <div style={styles.pageContainer}>{currentSlide + 1}/ {TOTAL_SLIDES + 1}</div>
-            </div>
-            {/* 오른쪽 화살표 div*/}
-            <NextBtn onClick={nextPage} />
-            {/* 필터 div*/}
-            <Filter result={result} getResult={getResult} />
+              {/* 버튼 div*/}
+              <div
+                style={{
+                  flexDirection: "column",
+                  width: "220px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {/* 방만들기 모달 */}
+                <ModalBase />
+                <br />
+                <Button onClick={randomEntry}>랜덤 입장</Button>
+              </div>
+            </RoomGrid>
+
+            {/* 방 & 필터 div*/}
+            <RoomGrid
+              is_flex_space
+              width="980px"
+              height="460px"
+              bg="#DAD4F6"
+              border="1px solid #DAD4F6"
+            >
+              {/* 왼쪽 화살표 div*/}
+              <PrevBtn onClick={prevPage} />
+              {/* 방 리스트 슬라이더 div*/}
+              <div style={styles.sliderContainer}>
+                <div style={styles.roomListContainer}>
+                  {rooms &&
+                    rooms.room_list.map((values) => {
+                      return values.room_status === "waiting" ? (
+                        <Room
+                          room_idx={values.room_idx}
+                          room_name={values.room_name}
+                          room_current_member={values.room_current_member_cnt}
+                          room_start_member={values.room_start_member_cnt}
+                          room_mode={values.room_mode}
+                          room_status={values.room_status}
+                          disabled="false"
+                          textStroke="true"
+                          cursor="true"
+                        />
+                      ) : (
+                        <Room
+                          room_idx={values.room_idx}
+                          room_name={values.room_name}
+                          room_current_member={values.room_current_member_cnt}
+                          room_start_member={values.room_start_member_cnt}
+                          room_mode={values.room_mode}
+                          room_status={values.room_status}
+                          disabled="true"
+                          textStroke="true"
+                          cursor="false"
+                        />
+                      );
+                    })}
+                  {createRoomData && (
+                    <Room
+                      room_idx={createRoomData.room_idx}
+                      room_name={createRoomData.room_name}
+                      room_current_member={
+                        createRoomData.room_current_member_cnt
+                      }
+                      room_start_member={createRoomData.room_start_member_cnt}
+                      room_mode={createRoomData.room_mode}
+                      room_status={createRoomData.room_status}
+                      disabled="false"
+                      textStroke="true"
+                      cursor="true"
+                    />
+                  )}
+                  {emptyRoomList()}
+                </div>
+              </div>
+              {/* 오른쪽 화살표 div*/}
+              <NextBtn onClick={nextPage} />
+              {/* 필터 div*/}
+              <Filter result={result} getResult={getResult} />
+            </RoomGrid>
+            <div style={styles.pageContainer}>
+                  {currentSlide + 1}/ {TOTAL_SLIDES + 1}
+                </div>
           </RoomGrid>
-        </RoomGrid>
+        ) : (
+          <RoomGrid
+            is_flex_center
+            flexDirection="row"
+            padding="20px"
+            width="1020px"
+            height="620px"
+            bg="#DAD4F6"
+          >
+            <Loading />
+          </RoomGrid>
+        )}
       </Background>
     </React.Fragment>
   );
@@ -506,7 +516,7 @@ const styles = {
     justifyContent: "space-between",
     flexDirection: "column",
     width: "680px",
-    height: "430px",
+    height: "410px",
     overflow: "hidden",
   },
   pageContainer: {
@@ -514,8 +524,9 @@ const styles = {
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "row",
-    width: "680px",
+    width: "770px",
     height: "15px",
     color: "#ffffff",
+    fontSize: "20px",
   },
 };
