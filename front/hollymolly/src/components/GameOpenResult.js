aa34056ac5a1a38743ca44448bae4b78adc399cf
jung_developer <kjhkjh0592@gmail.com>
2021-11-30 14:51:44 +0900

    [chore] 최종 마피아 공개 디자인 수정
    최종 마피아 공개 디자인 최종 수정

--- front/hollymolly/src/components/GameOpenResult.js
+++ front/hollymolly/src/components/GameOpenResult.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import style from '../styles/styles';
-import night from '../assets/night.svg';
-import PurpleCharacter from '../assets/purple.svg';
-import Human from '../assets/human.svg';
 import GameResultCard from '../components/GameResultCard';
+import UserOpenCard from './UserOpenCard';
 
 const GameFinalResult = ({ data }) => {
+    console.log('이름, 칼라 : ' + data.human_user_name + data.human_user_color)
     return (
         <React.Fragment>
             <Container>
                 <Title>인간 &nbsp; 공개</Title>
                 <HumanContainer>
-                    <GameResultCard role={'유령'} engRole={'GHOST'} final win></GameResultCard>
+                    {/* <GameResultCard role={'유령'} engRole={'GHOST'} final win></GameResultCard> */}
+                    <UserOpenCard nick = {data.human_user_name} color = {data.human_user_color}/>
                </HumanContainer>
+
                     <HumanWhoTxt>
                         인간 몰리는 <ResultSubtitle>{data.human_user_name} </ResultSubtitle>이였습니다.
                     </HumanWhoTxt>
@@ -42,12 +43,12 @@ const HumanContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    margin-top: 50px;
+    margin-top: 80px;
     margin-bottom: 15px;
 `;
 
 const Title = styled.text`
-    font-size: 55px;
+    font-size: 60px;
     font-family: Black Han Sans;
     -webkit-text-stroke: 1px #53305e;
     font-weight: bold;
@@ -55,6 +56,7 @@ const Title = styled.text`
     text-shadow: 4px 4px 0px #53305e, 5px 5px 0px #53305e, 6px 6px 0px #53305e, 7px 7px 0px #2a132e, 8px 8px 0px #2a132e,
         9px 9px 0px #2a132e, 10px 10px 0px #2a132e; //#2A132E
     margin-bottom: 20px;
+    margin-top: 30px;
 `;
 
 const ResultSubtitle = styled.text`
@@ -70,6 +72,7 @@ const HumanWhoTxt = styled.text`
     font-size: 25px;
     font-family: Gowun Dodum;
     color: #ffffff;
+    margin-top: 60px;
 `;
 
 export default GameFinalResult;
