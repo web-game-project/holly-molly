5614cc0909a91f45678def7a4bcc68ecf000fa1f
hailie <20181011@sungshin.ac.kr>
2021-12-05 00:35:26 +0900

    [update] moveRoom 로그 추가

--- back/socket/moveRoom.js
+++ back/socket/moveRoom.js
@@ -1,3 +1,5 @@
+const {printErrorLog, printLog} = require('../util/log');
+
 module.exports = (io, socket, roomIdToJoin) => {
     // move Room
     const currentRoom = Array.from(socket.rooms);
@@ -6,7 +8,7 @@ module.exports = (io, socket, roomIdToJoin) => {
         socket.leave(roomNum);
     }
     socket.join(roomIdToJoin);
-    
+    printLog('moveRoom', `${socket.user_idx}USER{${socket.id}, ${socket.rooms}}`);
     // send test message
     if(io){ 
         io.to(roomIdToJoin).emit('test', {roomIdToJoin, list:Array.from(socket.rooms)});
