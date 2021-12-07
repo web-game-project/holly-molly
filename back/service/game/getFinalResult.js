28a23fa3bf4c1a5a29e3df6b166ff5926d68af6d
jyeon1039 <20181039@sungshin.ac.kr>
2021-12-07 20:11:29 +0900

    [update] final result에 인간 정보 추가

--- back/service/game/getFinalResult.js
+++ back/service/game/getFinalResult.js
@@ -22,19 +22,22 @@ module.exports = async (req, res, next) => {
         // get final result
         const finalResult = await selectFinalResult(gameIdx);
         let result = await makeTotalScoreData(finalResult);
+        let human_info = await selectHuman(gameIdx);
+        result.human_color = human_info[0].wrm_user_color;
+        result.human_name = human_info[0].user_name;
         io.to(room_idx).emit('get final result', result);
+        console.log(room_idx);
 
         // finishGame
-        const gameMembers = await getGameMemberIndex(gameIdx);
+        /*const gameMembers = await getGameMemberIndex(gameIdx);
         await deleteAllAboutGame(gameMembers, gameIdx);
-        await updateRoomStatus(room_idx, 'waiting');
+        await updateRoomStatus(room_idx, 'waiting');*/
 
         // socket : change game status
         io.to(0).emit('change game status', {
             room_idx: game.get('room_room_idx'),
             room_status: 'waiting',
         });
-        
 
         res.status(204).end();
     } catch (error) {
@@ -48,38 +51,59 @@ module.exports = async (req, res, next) => {
 
 const selectFinalResult = async (gameIdx) => {
     const finalResult = await GameSet.findAll({
-        attributes: [
-            'game_set_human_score',
-            'game_set_ghost_score'
-        ],
+        attributes: ['game_set_human_score', 'game_set_ghost_score'],
         where: {
             game_game_idx: gameIdx,
         },
-        order: ['game_set_no']
+        order: ['game_set_no'],
     });
 
     return finalResult;
 };
 
+const selectHuman = async (game_idx) => {
+    var sql =
+        'SELECT wr.wrm_user_color, User.user_name ' +
+        'FROM GameMember as gm ' +
+        'JOIN WaitingRoomMember as wr ON wr.wrm_idx = gm.wrm_wrm_idx ' +
+        'JOIN User ON gm.wrm_user_idx = User.user_idx ' +
+        `WHERE gm.game_game_idx = ${game_idx} and gm.game_member_role = 'human'`;
+
+    const human = await sequelize.query(sql, {
+        type: sequelize.QueryTypes.SELECT,
+        raw: true,
+    });
+
+    return human;
+};
+
 const makeTotalScoreData = async (finalResult) => {
     let result = {};
-    result.one_game_set_human_score = finalResult[0].dataValues.game_set_human_score;
-    result.two_game_set_human_score = finalResult[1].dataValues.game_set_human_score;
-    result.three_game_set_human_score = finalResult[2].dataValues.game_set_human_score;
+    result.one_game_set_human_score =
+        finalResult[0].dataValues.game_set_human_score;
+    result.two_game_set_human_score =
+        finalResult[1].dataValues.game_set_human_score;
+    result.three_game_set_human_score =
+        finalResult[2].dataValues.game_set_human_score;
     result.total_game_set_human_score = 0;
-    for(let i in finalResult){
-        result.total_game_set_human_score += finalResult[i].dataValues.game_set_human_score;
+    for (let i in finalResult) {
+        result.total_game_set_human_score +=
+            finalResult[i].dataValues.game_set_human_score;
     }
-    result.one_game_set_ghost_score = finalResult[0].dataValues.game_set_ghost_score;
-    result.two_game_set_ghost_score = finalResult[1].dataValues.game_set_ghost_score;
-    result.three_game_set_ghost_score = finalResult[2].dataValues.game_set_ghost_score;
+    result.one_game_set_ghost_score =
+        finalResult[0].dataValues.game_set_ghost_score;
+    result.two_game_set_ghost_score =
+        finalResult[1].dataValues.game_set_ghost_score;
+    result.three_game_set_ghost_score =
+        finalResult[2].dataValues.game_set_ghost_score;
     result.total_game_set_ghost_score = 0;
-    for(let i in finalResult){
-        result.total_game_set_ghost_score += finalResult[i].dataValues.game_set_ghost_score;
+    for (let i in finalResult) {
+        result.total_game_set_ghost_score +=
+            finalResult[i].dataValues.game_set_ghost_score;
     }
 
     return result;
-}
+};
 
 const getGameMemberIndex = async (gameIdx) => {
     return await GameMember.findAll({
