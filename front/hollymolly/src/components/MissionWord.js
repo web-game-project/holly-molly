a6fc0278f670e21b24ea24cac20f648da371b2a9
JungYeonHee <dusgml5328@gmail.com>
2021-12-10 19:38:12 +0900

    [fix] QC_게임 중 미션 카드 디자인 수정

--- front/hollymolly/src/components/MissionWord.js
+++ front/hollymolly/src/components/MissionWord.js
@@ -3,21 +3,14 @@ import styled from "styled-components";
 
 const MissionWord = (props) => {
    
-    const {text, role, children} = props;
+    const {text, role} = props;
     
-
-    let forColor = "#FFFFFF"
-    if(role === 'human'){
-      forColor = "#FF0026"
-    }
-
-    const styles = {
-      color: forColor,
-    };
-
       return (
         <React.Fragment>
-              <MissionWordBox {...styles}>{text}</MissionWordBox>
+          {role === "ghost" ? 
+              <GhostMissionWordBox >{text}</GhostMissionWordBox> : 
+              <HumanMissionWordBox ><TextContainer>{text}</TextContainer></HumanMissionWordBox> 
+          }
         </React.Fragment>
       );
   };
@@ -27,14 +20,14 @@ const MissionWord = (props) => {
     backgroundColor: "#00ff00"
   };
 
-  const MissionWordBox = styled.div`
+  const GhostMissionWordBox = styled.div`
     text-align: center;
     font-family: "Black Han Sans";
     font-size: 25px;
     width: 150px;
     height: 85px;
     margin-bottom: 5px;
-    color: ${(props) => (props.color)}; 
+    color: white; 
     display: flex;
     justify-content: center;
     align-items: center;
@@ -43,4 +36,26 @@ const MissionWord = (props) => {
     border: 5px solid #FFBBC5;
   `;
 
+  const HumanMissionWordBox = styled.div`
+    text-align: center;
+    font-family: "Black Han Sans";
+    font-size: 25px;
+    width: 150px;
+    height: 85px;
+    margin-bottom: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0.5rem;
+    background: linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%);
+    border: 5px solid #FFBBC5;
+  `;
+
+  const TextContainer = styled.span `
+    font-family: "Black Han Sans";
+    background: linear-gradient(to bottom, #DB4843, #ff0000);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  `;
   export default MissionWord;
+
