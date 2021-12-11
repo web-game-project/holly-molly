e3fbc37819b640525e2a91d12d57ded50131b564
hailie <20181011@sungshin.ac.kr>
2021-12-11 16:27:47 +0900

    [update] 로그 추가

--- back/middleware/room.js
+++ back/middleware/room.js
@@ -1,5 +1,5 @@
 const { WaitingRoomMember } = require('../models');
-const {printErrorLog} = require('../util/log')
+const {printErrorLog, printLog} = require('../util/log')
 
 module.exports = async (req, res, next) => {
     // room_idx와 user 정보 가지고 방장/방원 체크
@@ -20,6 +20,7 @@ module.exports = async (req, res, next) => {
         });
 
         if (!roomMember) {
+            printLog('RoomMiddleware', realRoomIdx+"번방"+user.user_idx+"번유저");
             res.status(403).send({
                 message: '대기실/게임의 참여자가 아닙니다.',
             });
